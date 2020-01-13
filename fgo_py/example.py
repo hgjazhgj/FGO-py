7d314d757f08fc9421bd645d704e941971252edb
HGJ <huguangjing0411@geektip.cc>
2020-01-13 22:45:38 +0800

    v2.10.0 现在本脚本可用于任意分辨率

---
+++ fgo_py/example.py
@@ -0,0 +1,15 @@
+from fgo_py import *
+
+setSkillInfo('rider')
+oneBattle((0,1,2))
+#main()
+#main(0,2,danger=(0,1,2))
+#main(battleFunc=otk)
+#otk()
+
+#setSkillInfo('lancer')
+##oneBattle((0,0,1))
+#main(danger=(0,0,1))
+
+beep()
+
