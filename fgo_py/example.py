071d282d5e0a57dab356bc9b4523e4bd372a6326
hgjazhgj <hgjazhgj[at]gmail.com>
2020-01-16 00:15:34 +0800

    v2.10.3 重构&改动&优化

--- fgo_py/example.py
+++ fgo_py/example.py
@@ -1,21 +1,17 @@
 from fgo_py import *
-
-#setSkillInfo('rider')
-#main()
-#main(0,2,danger=(0,1,2))
-#main(battleFunc=otk)
-#otk()
-
-#setSkillInfo('lancer')
-##oneBattle((0,0,1))
-#main(danger=(0,0,1))
-
+import traceback
 try:
+    #draw()
+    print(getTime())
     setSkillInfo('lancer')
+    #oneBattle((0,0,1))
+    #main(danger=(0,0,1))
     #oneBattle((0,2,1))
-    main(150,0,danger=(0,2,1))
+    main(110,2,danger=(0,2,1))
 except BaseException as e:
     print(e)
+    traceback.print_exc()
 finally:
     playSound()
+    os.system("pause");
 
