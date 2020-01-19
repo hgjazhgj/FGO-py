122187ad0a72025dad7c67a4aa3429c07f0de649
HGJ <huguangjing0411@geektip.cc>
2020-01-19 15:18:35 +0800

    v2.10.4 重构,压缩,优化,自动化

--- fgo_py/fgo_shell.py
+++ fgo_py/fgo_shell.py
@@ -1,19 +1,18 @@
-from os import system as cmd
+import os
+import re
 
-cmd('adb connect localhost:5555')
+os.system('adb connect localhost:5555')
 adbPath='adb -s localhost:5555'
-dpx=0
 #adbPath='adb -s emulator-5554'
 #adbPath='adb -s 1e1b7921'
-#dpx=120
+with os.popen(adbPath+' shell wm size')as p:
+    pixelOffset,androidScale=(lambda size:((0,round(960*size[1]/size[0])-540),1920/size[0])if size[0]*1080<size[1]*1920else((round(540*size[0]/size[1])-960,0),1080/size[1]))((lambda x:[int(i)for i in x])(re.search('[0-9]{1,}x[0-9]{1,}',p.read()).group().split('x')))
 
-def tap(x,y):
-    cmd(adbPath+' shell input tap {} {}'.format(x+dpx,y))
-def swipe(rect,interval=500):
-    cmd(adbPath+' shell input swipe {} {} {} {} {}'.format(rect[0]+dpx,rect[1],rect[2]+dpx,rect[3],interval))
+tap=lambda x,y:os.system(adbPath+' shell input tap {} {}'.format(*[round(i*androidScale)for i in[x+pixelOffset[0],y+pixelOffset[1]]]))
+swipe=lambda rect,interval=500:os.system(adbPath+' shell input swipe {} {} {} {} {}'.format(*[round(i*androidScale)for i in[rect[0]+pixelOffset[0],rect[1]+pixelOffset[1],rect[2]+pixelOffset[0],rect[3]+pixelOffset[1]]],interval))
 
 #slnPath='E:/VisualStudioDocs/fgo_py/'
 #def screenShot(path=slnPath,name=''):
-#    cmd(adbPath+' shell screencap /sdcard/adbtemp/screen.png')
-#    cmd(adbPath+' pull /sdcard/adbtemp/screen.png "{path}ScreenShots/{name}.png"'.format(path=path,name=name if name!=''else time.strftime("%Y-%m-%d_%H.%M.%S",time.localtime())))
+#    os.system(adbPath+' shell screencap /sdcard/adbtemp/screen.png')
+#    os.system(adbPath+' pull /sdcard/adbtemp/screen.png "{path}ScreenShots/{name}.png"'.format(path=path,name=name if name!=''else time.strftime("%Y-%m-%d_%H.%M.%S",time.localtime())))
 
