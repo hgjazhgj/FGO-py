9b6032b7158fc0b0d80369fc0f03d9233c6901ab
HGJ <huguangjing0411@geektip.cc>
2020-01-19 15:18:35 +0800

    v2.10.4 重构,压缩,优化,自动化

--- fgo_py/example.py
+++ fgo_py/example.py
@@ -3,15 +3,16 @@ import traceback
 try:
     #draw()
     print(getTime())
-    setSkillInfo('lancer')
-    #oneBattle((0,0,1))
+    setInfo('lancer')
+    oneBattle((0,0,1))
     #main(danger=(0,0,1))
     #oneBattle((0,2,1))
-    main(110,2,danger=(0,2,1))
+    #main(0,1,danger=(0,2,1))
 except BaseException as e:
     print(e)
     traceback.print_exc()
 finally:
+    print(getTime())
     playSound()
     os.system("pause");
 
--- fgo_py/fgo_py.py
+++ fgo_py/fgo_py.py
@@ -49,25 +49,13 @@
 #                                                .\@OOOOO@@OO@\     ,@O@@OO@@OO@@\@                                    
 #                                                   ,[\[[[`.          .[\@OOOOO@^/^                                    
 #                                                                         ,@]]@@O@^                                    
-
-'full-automatic FGO script'
+'Full-automatic FGO Script'
 __auther__='hgjazhgj'
-
-import time
-import os
-import numpy
-import cv2
-import win32con
-import win32ui
-import win32gui
-import winsound
-
+import time,os,numpy,cv2,win32con,win32ui,win32gui,win32print,winsound
 from fgo_shell import *
-
 slnPath='E:/VisualStudioDocs/fgo_py/'
-scale=1.25
 hWnd=win32gui.FindWindowEx(win32gui.FindWindow(None,'BlueStacks App Player'),None,None,None)#'BlueStacks Android PluginAndroid'
-
+winScale=(lambda hDC:(win32print.GetDeviceCaps(hDC,win32con.DESKTOPHORZRES)/win32print.GetDeviceCaps(hDC,win32con.HORZRES),win32gui.ReleaseDC(None,hDC))[0])(win32gui.GetDC(0))
 IMG_APEMPTY=cv2.imread(slnPath+'asserts/apempty.png')
 IMG_ATTACK=cv2.imread(slnPath+'asserts/attack.png')
 IMG_BEGIN=cv2.imread(slnPath+'asserts/begin.png')
@@ -82,25 +70,9 @@ IMG_HOUGUSEALED=cv2.imread(slnPath+'asserts/hougusealed.png')
 IMG_NOFRIEND=cv2.imread(slnPath+'asserts/nofriend.png')
 IMG_STILL=cv2.imread(slnPath+'asserts/still.png')
 IMG_STAGE=[cv2.imread(slnPath+'asserts/stage/'+file)for file in os.listdir(slnPath+'asserts/stage')if file.endswith('.png')]
-
-friendPos=4
-skillInfo=[[[4,0,0],[4,0,0],[4,0,0]],[[4,0,0],[4,0,0],[4,0,0]],[[4,0,0],[4,0,0],[4,0,0]],[[4,0,0],[4,0,0],[4,0,0]],[[4,0,0],[4,0,0],[4,0,0]],[[4,0,0],[4,0,0],[4,0,0]]]
-#skillInfo=[#minstage,minstageturn,obj
-#    [[1,2,0],[1,0,0],[4,0,0]],
-#    [[1,0,0],[1,0,0],[1,0,0]],
-#    [[1,0,0],[1,0,0],[3,3,0]],
-#    [[4,0,0],[4,0,0],[4,0,0]],
-#    [[4,0,0],[4,0,0],[4,0,0]],
-#    [[4,0,0],[4,0,0],[4,0,0]]]
-#houguInfo=[[1,0],[1,0],[1,0],[1,1],[1,1],[1,1]]#minstage,priority
-houguInfo=[[2,1],[3,1],[3,1],[1,1],[1,1],[1,1]]#minstage,priority
-houguInfo[friendPos]=[3,0]
-
-def rangeInf(start=0,step=1):
-    i=start
-    while True:
-        yield i
-        i+=step
+skillInfo=[[[4,0,0],[4,0,0],[4,0,0]],[[4,0,0],[4,0,0],[4,0,0]],[[4,0,0],[4,0,0],[4,0,0]],[[4,0,0],[4,0,0],[4,0,0]],[[4,0,0],[4,0,0],[4,0,0]],[[4,0,0],[4,0,0],[4,0,0]]]#minstage,minstageturn,obj
+houguInfo=[[1,1],[1,1],[1,1],[1,1],[1,1],[1,1]]#minstage,priority
+friendPos=5
 class Fuse(object):
     def __init__(self,fv=300):
         self.__value=0
@@ -120,140 +92,48 @@ class Fuse(object):
     def show(self):
         print(self.__value,'/',self.__max,sep='',flush=True)
 fuse=Fuse()
-def rgb2hsv(x):
-    '''
-    R,G,B:[0,255]
-    H:[0,359]
-    S,V:[0,100]
-    '''
-    R=int(x[2])
-    G=int(x[1])
-    B=int(x[0])
-    cmax=max(R,G,B)
-    delta=cmax-min(R,G,B)
-    H=0if delta==0else((G-B)/delta if R==cmax else(B-R)/delta+2if G==cmax else(R-G)/delta+4)*60%360
-    S=0if cmax==0else delta/cmax
-    V=cmax
-    return(int(H),int(100*S),int(V*100/255))
-def press(c):
-    tap(*{
+rgb2hsv=lambda x:(lambda R,G,B:(lambda cmax:(lambda delta:(0if delta==0else int(((G-B)/delta if R==cmax else(B-R)/delta+2if G==cmax else(R-G)/delta+4)*60)%360,0if cmax==0else int(100*delta/cmax),int(cmax*100/255)))(cmax-min(R,G,B)))(max(R,G,B)))(*x[2::-1])#R,G,B:[0,255]/H:[0,359]/S,V:[0,100]
+press=lambda c:tap(*{
         '\x09':(1800,304),#tab VK_TAB
         '\x12':(960,943),#alt VK_MENU
-        ' ':(1820,1030),
-        '0':(70,69),
-        '1':(277,640),
-        '2':(648,640),
-        '3':(974,640),
-        '4':(1262,640),
-        '5':(1651,640),
-        '6':(646,304),
-        '7':(976,304),
-        '8':(1267,304),
-        'A':(109,860),
-        'B':(1680,368),
-        'C':(845,540),
-        'D':(385,860),
-        'E':(1493,470),
-        'F':(582,860),
-        'G':(724,860),
-        'H':(861,860),
-        'J':(1056,860),
-        'K':(1201,860),
-        'L':(1336,860),
-        'N':(248,1041),
-        'P':(1854,69),
-        'Q':(1800,475),
-        'R':(1626,475),
-        'S':(244,860),
-        'V':(1105,540),
-        'W':(1360,475),
-        'X':(259,932),
+' ':(1820,1030),'0':(70,69),'1':(277,640),'2':(648,640),'3':(974,640),'4':(1262,640),'5':(1651,640),'6':(646,304),
+'7':(976,304),'8':(1267,304),'A':(109,860),'B':(1680,368),'C':(845,540),'D':(385,860),'E':(1493,470),'F':(582,860),
+'G':(724,860),'H':(861,860),'J':(1056,860),'K':(1201,860),'L':(1336,860),'N':(248,1041),'P':(1854,69),'Q':(1800,475),
+'R':(1626,475),'S':(244,860),'V':(1105,540),'W':(1360,475),'X':(259,932),
         '\xA0':(41,197),# VK_LSHIFT
         '\xA1':(41,197),# VK_RSHIFT
         '\xBA':(1247,197),#; VK_OEM_1
         '\xBB':(791,69),#+= VK_OEM_PLUS
         '\xBD':(427,69),#-_ VK_OEM_MINUS
     }[c])
-def doit(touch,wait):
-    for i in range(len(touch)):
-        press(touch[i])
-        time.sleep(wait[i]/1000)
-def beep():
-    winsound.PlaySound('SystemHand',0)
-def show(img):
-    cv2.imshow('imshow',img)
-    cv2.waitKey()
-    cv2.destroyAllWindows()
-def windowCapture(hWnd=hWnd):
-    hWndDC=win32gui.GetWindowDC(hWnd)
-    left,top,right,bot=win32gui.GetWindowRect(hWnd)
-    width=int((right-left)*scale+.001)
-    height=int((bot-top)*scale+.001)
-    mfcDC=win32ui.CreateDCFromHandle(hWndDC)
-    saveDC=mfcDC.CreateCompatibleDC()
-    saveBitMap=win32ui.CreateBitmap()
-    saveBitMap.CreateCompatibleBitmap(mfcDC,width,height)
-    saveDC.SelectObject(saveBitMap)
-    saveDC.BitBlt((0, 0),(width,height),mfcDC,(0,0),win32con.SRCCOPY)
-    img=numpy.frombuffer(saveBitMap.GetBitmapBits(True),dtype='uint8').reshape(height,width,4)[:,:,0:3]
-    win32gui.DeleteObject(saveBitMap.GetHandle())
-    saveDC.DeleteDC()
-    mfcDC.DeleteDC()
-    win32gui.ReleaseDC(hWnd,hWndDC)
-    return img
-def playSound(file=slnPath+'sound/default.wav',flag=winsound.SND_LOOP|winsound.SND_ASYNC):
-    winsound.PlaySound(file,flag)
-    os.system('pause')
-    winsound.PlaySound(None,0)
-def getTime():
-    return time.strftime('%Y-%m-%d_%H.%M.%S',time.localtime())
-
+doit=lambda touch,wait:[(press(touch[i]),time.sleep(wait[i]*.001))for i in range(len(touch))]
+beep=lambda:winsound.PlaySound('SystemHand',0)
+show=lambda img:(cv2.imshow('imshow',img),cv2.waitKey(),cv2.destroyAllWindows())
+windowCapture=lambda hWnd=hWnd:(lambda width,height:(lambda hWndDC:(lambda mfcDC:(lambda memDC,bitMap:(bitMap.CreateCompatibleBitmap(mfcDC,width,height),memDC.SelectObject(bitMap),memDC.BitBlt((0, 0),(width,height),mfcDC,(0,0),win32con.SRCCOPY),numpy.frombuffer(bitMap.GetBitmapBits(True),dtype='uint8').reshape(height,width,4)[:,:,0:3],win32gui.DeleteObject(bitMap.GetHandle()),memDC.DeleteDC(),mfcDC.DeleteDC(),win32gui.ReleaseDC(hWnd,hWndDC))[3])(mfcDC.CreateCompatibleDC(),win32ui.CreateBitmap()))(win32ui.CreateDCFromHandle(hWndDC)))(win32gui.GetDC(hWnd)))(*(lambda left,top,right,bot:[int(winScale*i+.001)for i in[right-left,bot-top]])(*win32gui.GetClientRect(hWnd)))
+playSound=lambda file=slnPath+'sound/default.wav',flag=winsound.SND_LOOP|winsound.SND_ASYNC:(winsound.PlaySound(file,flag),os.system('pause'),winsound.PlaySound(None,0))
+getTime=lambda:time.strftime('%Y-%m-%d_%H.%M.%S',time.localtime())
 class Check(object):
     def __init__(self):
         fuse.increase()
-        #screenShot(name='chk')
-        #self.im=cv2.imread(slnPath+'ScreenShots/chk.png')[0:1080,dpx:dpx+1920]
         time.sleep(.08)
-        self.im=cv2.resize(windowCapture(),(1920,1080),interpolation=cv2.INTER_CUBIC)
-    def compare(self,img,rect=(0,0,1920,1080),delta=.03):
-        return cv2.minMaxLoc(cv2.matchTemplate(self.im[rect[1]:rect[3],rect[0]:rect[2]],img,cv2.TM_SQDIFF_NORMED))[0]<delta
-    def select(self,img,rect=(0,0,1920,1080)):
-        return (lambda x:x.index(min(x)))([cv2.minMaxLoc(cv2.matchTemplate(self.im[rect[1]:rect[3],rect[0]:rect[2]],i,cv2.TM_SQDIFF_NORMED))[0]for i in img])
-    def tapOnCmp(self,img,rect=(0,0,1920,1080),delta=.03):
-        loc=cv2.minMaxLoc(cv2.matchTemplate(self.im[rect[1]:rect[3],rect[0]:rect[2]],img,cv2.TM_SQDIFF_NORMED))
-        if loc[0]>=delta:
-            return False
-        tap(rect[0]+loc[2][0]+img.shape[1]//2,rect[1]+loc[2][1]+img.shape[0]//2)
-        time.sleep(.5)
-        return fuse.reset()
-    def save(self,name=''):
-        cv2.imwrite(slnPath+'ScreenShots/'+(getTime()+'.png'if name==''else name),self.im)
-        return self
-    def isTurnBegin(self):
-        return self.compare(IMG_ATTACK,(1567,932,1835,1064))and fuse.reset()
-    def isBattleOver(self):
-        return(self.compare(IMG_BOUND,(95,235,460,318))or self.compare(IMG_BOUNDUP,(978,517,1491,596),.06))and fuse.reset()
-    def isBegin(self):
-        return self.compare(IMG_BEGIN,(1630,950,1919,1079))and fuse.reset()
-    def isHouguReady(self):
-        return[rgb2hsv(self.im[1002][290+480*i])[1]>5for i in range(3)]
-    def isHouguSealed(self):
-        return[any([self.compare(j,(470+346*i,258,768+346*i,387),.3)for j in(IMG_HOUGUSEALED,IMG_CARDSEALED)])for i in range(3)]
-    def isSkillReady(self):
-        return[[not self.compare(IMG_STILL,(65+480*i+141*j,895,107+480*i+141*j,927),.06)for j in range(3)]for i in range(3)]
-    def isApEmpty(self):
-        return self.compare(IMG_APEMPTY,(800,50,1120,146))and fuse.reset()
-    def isChooseFriend(self):
-        return self.compare(IMG_CHOOSEFRIEND,(1628,314,1772,390))and fuse.reset()
-    def isNoFriend(self):
-        return self.compare(IMG_NOFRIEND,(369,545,1552,797),.1)and fuse.reset()
-    def getABQ(self):
-        return[-1if self.compare(IMG_CARDSEALED,(43+386*i,667,345+386*i,845),.3)else(lambda x:x.index(max(x)))((lambda x:[int(numpy.mean([j[i]for k in x for j in k]))for i in(2,1,0)])(self.im[771:919,108+386*i:318+386*i]))for i in range(5)]
-    def getStage(self):
-        return self.select(IMG_STAGE,(1290,14,1348,60))+1
-    def getPortrait(self):
-        return[self.im[640:740,195+480*i:296+480*i]for i in range(3)]
-
+        self.im=(lambda im:im[im.shape[0]//2-540:im.shape[0]//2+540,im.shape[1]//2-960:im.shape[1]//2+960])((lambda img:(lambda scale:cv2.resize(img,(0,0),None,scale,scale,cv2.INTER_CUBIC))(max(1920/img.shape[1],1080/img.shape[0])))(windowCapture()))
+    compare=lambda self,img,rect=(0,0,1920,1080),delta=.03:cv2.minMaxLoc(cv2.matchTemplate(self.im[rect[1]:rect[3],rect[0]:rect[2]],img,cv2.TM_SQDIFF_NORMED))[0]<delta
+    select=lambda self,img,rect=(0,0,1920,1080):(lambda x:x.index(min(x)))([cv2.minMaxLoc(cv2.matchTemplate(self.im[rect[1]:rect[3],rect[0]:rect[2]],i,cv2.TM_SQDIFF_NORMED))[0]for i in img])
+    tapOnCmp=lambda self,img,rect=(0,0,1920,1080),delta=.03:(lambda loc:loc[0]<delta and(tap(rect[0]+loc[2][0]+img.shape[1]//2,rect[1]+loc[2][1]+img.shape[0]//2),time.sleep(.5),fuse.reset())[2])(cv2.minMaxLoc(cv2.matchTemplate(self.im[rect[1]:rect[3],rect[0]:rect[2]],img,cv2.TM_SQDIFF_NORMED)))
+    seve=lambda self,name='':(cv2.imwrite(slnPath+'ScreenShots/'+(getTime()+'.png'if name==''else name),self.im),self)[1]
+    show=lambda self:(show(cv2.resize(self.im,(0,0),None,.5,.5)),self)[1]
+    isTurnBegin=lambda self:self.compare(IMG_ATTACK,(1567,932,1835,1064))and fuse.reset()
+    isBattleOver=lambda self:(self.compare(IMG_BOUND,(95,235,460,318))or self.compare(IMG_BOUNDUP,(978,517,1491,596),.06))and fuse.reset()
+    isBegin=lambda self:self.compare(IMG_BEGIN,(1630,950,1919,1079))and fuse.reset()
+    isHouguReady=lambda self:[rgb2hsv(self.im[1002][280+480*i])[1]>7for i in range(3)]
+    isHouguSealed=lambda self:[any([self.compare(j,(470+346*i,258,768+346*i,387),.3)for j in(IMG_HOUGUSEALED,IMG_CARDSEALED)])for i in range(3)]
+    isSkillReady=lambda self:[[not self.compare(IMG_STILL,(65+480*i+141*j,895,107+480*i+141*j,927),.06)for j in range(3)]for i in range(3)]
+    isApEmpty=lambda self:self.compare(IMG_APEMPTY,(800,50,1120,146))and fuse.reset()
+    isChooseFriend=lambda self:self.compare(IMG_CHOOSEFRIEND,(1628,314,1772,390))and fuse.reset()
+    isNoFriend=lambda self:self.compare(IMG_NOFRIEND,(369,545,1552,797),.1)and fuse.reset()
+    getABQ=lambda self:[-1if self.compare(IMG_CARDSEALED,(43+386*i,667,345+386*i,845),.3)else(lambda x:x.index(max(x)))((lambda x:[int(numpy.mean([j[i]for k in x for j in k]))for i in(2,1,0)])(self.im[771:919,108+386*i:318+386*i]))for i in range(5)]
+    getStage=lambda self:self.select(IMG_STAGE,(1290,14,1348,60))+1
+    getPortrait=lambda self:[self.im[640:740,195+480*i:296+480*i]for i in range(3)]
 def chooseFriend():
     if len(IMG_FRIEND)==0:
         doit('8',(1000,))
@@ -277,50 +157,31 @@ def chooseFriend():
             swipe((220,960,220,457))
             time.sleep(.2)
         doit('\xBAJ',(500,1000))
-
 def draw():
     while True:
-        doit('2',(20,))
-
-def setSkillInfo(s):
-    if s=='saber':#muzashi/modoredo/okita
-        skillInfo[0]=[[1,0,0],[1,0,0],[3,5,0]]
-        skillInfo[1]=[[1,0,0],[1,2,0],[1,0,0]]
-        skillInfo[2]=[[1,0,0],[1,2,0],[3,5,0]]
-    elif s=='archer':#arutoria/erio/atera
-        skillInfo[0]=[[1,0,0],[4,0,0],[1,0,0]]
-        skillInfo[1]=[[1,0,0],[3,0,0],[3,5,0]]
-        skillInfo[2]=[[1,0,1],[1,0,2],[4,0,0]]
-    elif s=='lancer':#ere/tamamo/jyanu
-        skillInfo[0]=[[3,3,0],[1,0,0],[2,0,0]]
-        skillInfo[1]=[[1,0,0],[3,0,0],[3,3,0]]
-        skillInfo[2]=[[3,0,1],[1,0,0],[3,3,0]]
-    elif s=='rider':#arutoria/asutorufo/maruta
-        skillInfo[0]=[[3,0,1],[2,0,0],[1,0,0]]
-        skillInfo[1]=[[1,0,0],[3,0,0],[1,0,0]]
-        skillInfo[2]=[[3,0,0],[3,0,0],[3,0,0]]
-    elif s=='caster':#malin/girugyameshi/mari
-        skillInfo[0]=[[1,0,0],[3,4,0],[1,0,2]]
-        skillInfo[1]=[[1,0,0],[1,0,0],[1,0,0]]
-        skillInfo[2]=[[1,0,0],[1,0,0],[1,0,0]]
-    elif s=='assassin':#kurobatera/hiroinx/jakku
-        skillInfo[0]=[[1,0,0],[1,0,0],[3,6,0]]
-        skillInfo[1]=[[2,0,0],[3,6,0],[4,0,0]]
-        skillInfo[2]=[[3,6,0],[3,2,0],[3,0,3]]
-    elif s=='ex':#hokusai/bb/hiroinx
-        skillInfo[0]=[[1,0,0],[1,0,0],[1,0,0]]
-        skillInfo[1]=[[1,2,0],[1,0,0],[4,0,0]]
-        skillInfo[2]=[[3,6,0],[2,0,0],[1,0,0]]
-
+        press('2')
+def setInfo(s):
+    skillInfo[:3]={
+        'saber'   :[[[1,0,0],[1,0,0],[3,5,0]],[[1,0,0],[1,2,0],[1,0,0]],[[1,0,0],[1,2,0],[3,5,0]]],#muzashi/modoredo/okita
+        'archer'  :[[[1,0,0],[4,0,0],[1,0,0]],[[1,0,0],[3,0,0],[3,5,0]],[[1,0,1],[1,0,2],[4,0,0]]],#arutoria/erio/atera
+        'lancer'  :[[[3,3,0],[1,0,0],[2,0,0]],[[1,0,0],[3,0,0],[3,3,0]],[[3,0,1],[1,0,0],[3,3,0]]],#ere/tamamo/jyanu
+        'rider'   :[[[3,0,1],[2,0,0],[1,0,0]],[[1,0,0],[3,0,0],[1,0,0]],[[3,0,0],[3,0,0],[3,0,0]]],#arutoria/asutorufo/maruta
+        'caster'  :[[[1,0,0],[3,4,0],[1,0,2]],[[1,0,0],[1,0,0],[1,0,0]],[[1,0,0],[1,0,0],[1,0,0]]],#malin/girugyameshi/mari
+        'assassin':[[[1,0,0],[1,0,0],[3,6,0]],[[2,0,0],[3,6,0],[4,0,0]],[[3,6,0],[3,2,0],[3,0,3]]],#kurobatera/hiroinx/jakku
+        'ex'      :[[[1,0,0],[1,0,0],[1,0,0]],[[1,2,0],[1,0,0],[4,0,0]],[[3,6,0],[2,0,0],[1,0,0]]],#hokusai/bb/hiroinx
+    }[s]
+    global friendPos
+    friendPos=4
+    houguInfo[:]=[[2,1],[3,1],[3,1],[1,1],[1,1],[1,1]]
+    houguInfo[friendPos]=[3,0]
 def oneBattle(danger=(0,0,1)):
     turn,stage,stageTurn,servant=0,0,0,[0,1,2]
     while True:
         chk=Check()
         if chk.isTurnBegin():
             time.sleep(.3)
-            turn+=1
-            stage,stageTurn,skill,newPort=(lambda chk:(lambda x:[x,stageTurn+1if stage==x else 1])(chk.getStage())+[chk.isSkillReady(),chk.getPortrait()])(Check())
-            print('    ',turn,stage,stageTurn,getTime())
+            turn,stage,stageTurn,skill,newPort=[turn+1]+(lambda chk:(lambda x:[x,stageTurn+1if stage==x else 1])(chk.getStage())+[chk.isSkillReady(),chk.getPortrait()])(Check())
+            print('    ',turn,stage,stageTurn)
             if stageTurn==1:
                 doit('\xBB\xBD0'[danger[stage-1]]+'P',(50,500))
             if turn==1:
@@ -339,7 +200,7 @@ def oneBattle(danger=(0,0,1)):
                     time.sleep(.2)
             hougu=(lambda x:[servant[i]<6and stage>=houguInfo[servant[i]][0]and any([x[j][i]for j in range(len(x))])for i in range(3)])((Check().isHouguReady(),Check().isHouguReady(),Check().isHouguReady()))
             doit(' ',(2700,))
-            doit((lambda chk:(lambda h,c:([chr(i+54)for pri in{houguInfo[i][1]for i in servant if i<6}for i in range(3)if h[i]and houguInfo[servant[i]][1]==pri]if any(h)else[chr(j+49)for i in range(3)if c.count(i)>=3for j in range(5)if c[j]==i])+[chr(j+49)for i in(0,2,1,-1)for j in range(5)if c[j]==i])((lambda x,y:[x[i]^y[i]for i in range(3)])(hougu,chk.isHouguSealed()),chk.getABQ()))(Check())[:3],(80,80,10000))
+            doit((lambda chk:(lambda h,c:([chr(i+54)for pri in{houguInfo[i][1]for i in servant if i<6}for i in range(3)if h[i]and houguInfo[servant[i]][1]==pri]if any(h)else[chr(j+49)for i in range(3)if c.count(i)>=3for j in range(5)if c[j]==i])+[chr(j+49)for i in(0,2,1,-1)for j in range(5)if c[j]==i])((lambda x,y:[x[i]and not y[i]for i in range(3)])(hougu,chk.isHouguSealed()),chk.getABQ()))(Check())[:3],(100,100,10000))
         elif chk.isBattleOver():
             print('  Battle Finished',getTime())
             break
@@ -350,10 +211,11 @@ def oneBattle(danger=(0,0,1)):
         else:
             time.sleep(.2)
     return True
-
 def main(appleCount=0,appleKind=0,battleFunc=oneBattle,*args,**kwargs):
     apple=appleCount
-    for i in rangeInf(1):
+    battle=0
+    while True:
+        battle+=1
         doit('8',(1000,))
         if Check().isApEmpty():
             if apple==0:
@@ -364,7 +226,7 @@ def main(appleCount=0,appleKind=0,battleFunc=oneBattle,*args,**kwargs):
                 doit('W4K8'[appleKind]+'L',(400,1200))
                 apple-=1
                 print('Apple',appleCount-apple)
-        print('  Battle',i,getTime())
+        print('  Battle',battle,getTime())
         flush=True
         while True:
             chk=Check()
@@ -379,10 +241,11 @@ def main(appleCount=0,appleKind=0,battleFunc=oneBattle,*args,**kwargs):
         if not battleFunc(*args,**kwargs):
             doit('VJ',(500,500))
         doit('        ',(200,200,200,200,200,200,200,200))
+        flush=True
         while True:
             chk=Check()
             if chk.isBegin():
                 break;
-            chk.tapOnCmp(IMG_END,rect=(243,863,745,982))
+            if flush and chk.tapOnCmp(IMG_END,rect=(243,863,745,982)):
+                flush=False
             doit(' ',(200,))
-
