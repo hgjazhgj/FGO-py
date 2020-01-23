3320ce2cb22a471c92d0fdd27c1e0fd2fbce1a0f
hgjazhgj <hgjazhgj[at]gmail.com>
2020-01-23 16:18:55 +0800

    v2.10.8 (负)优化

--- fgo_py/example.py
+++ fgo_py/example.py
@@ -1,17 +1,21 @@
 from fgo_py import *
 import traceback
+show(windowCapture())
+Check().show()
+exit(0)
 try:
     #draw()
     #show(windowCapture())
     print(getTime())
-    setInfo('saber')
+    setInfo('archer')
     oneBattle()
     #main(danger=(0,0,1))
     #oneBattle((0,2,1))
     #main(0,0,danger=(0,2,1))
 except BaseException as e:
-    print(e)
-    traceback.print_exc()
+    if type(e)!=SystemExit:
+        print(e)
+        traceback.print_exc()
 finally:
     print(getTime())
     playSound()
