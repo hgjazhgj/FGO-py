a2cec05b7cb91fc15da2604e043d5e759ccb2a77
hgjazhgj <hgjazhgj[at]gmail.com>
2020-03-16 22:09:46 +0800

    v3.6.0 airtest.javacap部分机型兼容优化

--- fgoFunc.py
+++ fgoFunc.py
@@ -49,16 +49,23 @@ class Base(Android):
         except:
             self.serialno=None
             return
-        self.tapOffset,self.tapScale=(lambda size:((0,round(960*size[0]/size[1])-540),1920/size[1])if size[1]*1080<size[0]*1920else((round(540*size[1]/size[0])-960,0),1080/size[0]))(sorted(self.get_current_resolution()))
-        self.key={c:[round((p[i]+self.tapOffset[i])/self.tapScale)for i in range(2)]for c,p in
+        self.size=tuple(sorted(self.get_current_resolution(),reverse=True))
+        if self.size[0]*9>self.size[1]*16:
+            self.tapScale=1080/self.size[1]
+            self.tapOffset=(round(self.size[0]-self.size[1]*16/9)>>1,0)
+        else:
+            self.tapScale=1920/self.size[0]
+            self.tapOffset=(0,round(self.size[0]*9/16-self.size[1])>>1)
+        self.key={c:[round(p[i]/self.tapScale+self.tapOffset[i])for i in range(2)]for c,p in
            {' ':(1820,1030),'1':(277,640),'2':(648,640),'3':(974,640),'4':(1262,640),'5':(1651,640),'6':(646,304),'7':(976,304),'8':(1267,304),
             'A':(109,860),'B':(1680,368),'C':(845,540),'D':(385,860),'E':(1493,470),'F':(582,860),'G':(724,860),'H':(861,860),'J':(1056,860),'K':(1201,860),
             'L':(1336,860),'N':(248,1041),'P':(1854,69),'Q':(1800,475),'R':(1626,475),'S':(244,860),'V':(1105,540),'W':(1360,475),'X':(259,932),
             '\x64':(70,221),'\x65':(427,221),'\x66':(791,221),'\x67':(70,69),'\x68':(427,69),'\x69':(791,69),#NUM4 #NUM5 #NUM6 #NUM7 #NUM8 #NUM9
             '\x09':(1800,304),'\x12':(960,943),'\xA0':(41,197),'\xA1':(41,197),'\xBA':(1247,197)}.items()}# VK_LSHIFT # VK_RSHIFT #; VK_OEM_1 #tab VK_TAB #alt VK_MENU
-    def tap(self,x,y):self.touch([round(i*self.tapScale)for i in[x+self.tapOffset[0],y+self.tapOffset[1]]])
-    def swipe(self,rect):super().swipe(*[[round((rect[(i<<1)+j]+self.tapOffset[j])*self.tapScale)for j in range(2)]for i in range(2)])
-    def press(self,c):self.touch(self.key[c])
+    def touch(self,p):super().touch([round(p[i]*self.tapScale+self.tapOffset[i])for i in range(2)])
+    def swipe(self,rect):super().swipe(*[[round(rect[i<<1|j]*self.tapScale)+self.tapOffset[j]for j in range(2)]for i in range(2)])
+    def press(self,c):super().touch(self.key[c])
+    def snapshot(self):return cv2.resize(super().snapshot(),self.size,interpolation=cv2.INTER_CUBIC)
 base=Base()
 def doit(touch,wait):[(base.press(i),time.sleep(j*.001))for i,j in zip(touch,wait)]
 class Check:
@@ -69,21 +76,21 @@ class Check:
         time.sleep(lagency)
         fuse.increase()
         #self.im=(lambda im:im[(im.shape[0]>>1)-540:(im.shape[0]>>1)+540,(im.shape[1]>>1)-960:(im.shape[1]>>1)+960])((lambda scale:cv2.resize(img,(0,0),None,scale,scale,cv2.INTER_CUBIC))(max(1920/img.shape[1],1080/img.shape[0])))
-        self.im=cv2.resize(base.snapshot()[base.tapOffset[1]:-base.tapOffset[1]if base.tapOffset[1]else None,base.tapOffset[0]:-base.tapOffset[0]if base.tapOffset[0]else None],(0,0),None,base.tapScale,base.tapScale,cv2.INTER_CUBIC)
-    def compare(self,img,rect=(0,0,1920,1080),delta=.03):return cv2.minMaxLoc(cv2.matchTemplate(self.im[rect[1]:rect[3],rect[0]:rect[2]],img,cv2.TM_SQDIFF_NORMED))[0]<delta
+        self.im=cv2.resize(base.snapshot()[base.tapOffset[1]:-base.tapOffset[1]if base.tapOffset[1]else None,base.tapOffset[0]:-base.tapOffset[0]if base.tapOffset[0]else None],(1920,1080),interpolation=cv2.INTER_CUBIC)
+    def compare(self,img,rect=(0,0,1920,1080),delta=.05):return cv2.minMaxLoc(cv2.matchTemplate(self.im[rect[1]:rect[3],rect[0]:rect[2]],img,cv2.TM_SQDIFF_NORMED))[0]<delta
     def select(self,img,rect=(0,0,1920,1080)):return(lambda x:x.index(min(x)))([cv2.minMaxLoc(cv2.matchTemplate(self.im[rect[1]:rect[3],rect[0]:rect[2]],i,cv2.TM_SQDIFF_NORMED))[0]for i in img])
-    def tapOnCmp(self,img,rect=(0,0,1920,1080),delta=.03):return(lambda loc:loc[0]<delta and(base.tap(rect[0]+loc[2][0]+img.shape[1]//2,rect[1]+loc[2][1]+img.shape[0]//2),time.sleep(.5),fuse.reset())[2])(cv2.minMaxLoc(cv2.matchTemplate(self.im[rect[1]:rect[3],rect[0]:rect[2]],img,cv2.TM_SQDIFF_NORMED)))
-    def save(self,name=''):cv2.imwrite('ScreenShots/'+(getTime()+'.png'if name==''else name),self.im);return self
-    def show(self):show(cv2.resize(self.im,(800,450)));return self
+    def tapOnCmp(self,img,rect=(0,0,1920,1080),delta=.05):return(lambda loc:loc[0]<delta and(base.touch(rect[0]+loc[2][0]+img.shape[1]//2,rect[1]+loc[2][1]+img.shape[0]//2),time.sleep(.5),fuse.reset())[2])(cv2.minMaxLoc(cv2.matchTemplate(self.im[rect[1]:rect[3],rect[0]:rect[2]],img,cv2.TM_SQDIFF_NORMED)))
+    def save(self,name=''):cv2.imwrite(getTime()+'.jpg'if name==''else name,self.im);return self
+    def show(self):show(cv2.resize(self.im,(0,0),None,.4,.4,cv2.INTER_NEAREST));return self
     def isTurnBegin(self):return self.compare(IMG_ATTACK,(1567,932,1835,1064))and fuse.reset()
     def isBattleOver(self):return(self.compare(IMG_BOUND,(95,235,460,318))or self.compare(IMG_BOUNDUP,(978,517,1491,596),.06))and fuse.reset()
     def isBegin(self):return self.compare(IMG_BEGIN,(1630,950,1919,1079))and fuse.reset()
-    def isHouguReady(self):return(lambda im:[not any([self.compare(j,(470+346*i,258,768+346*i,387),.3)for j in(IMG_HOUGUSEALED,IMG_CARDSEALED)])and(numpy.mean(self.im[1014:1021,217+480*i:235+480*i])>90or numpy.mean(im[1014:1021,217+480*i:235+480*i])>90)for i in range(3)])(Check(.8).im)
-    def isSkillReady(self):return[[not self.compare(IMG_STILL,(65+480*i+141*j,895,107+480*i+141*j,927),.06)for j in range(3)]for i in range(3)]
+    def isHouguReady(self):return(lambda im:[not any([self.compare(j,(470+346*i,258,768+346*i,387))for j in(IMG_HOUGUSEALED,IMG_CARDSEALED)])and(numpy.mean(self.im[1014:1021,217+480*i:235+480*i])>90or numpy.mean(im[1014:1021,217+480*i:235+480*i])>90)for i in range(3)])(Check(.8).im)
+    def isSkillReady(self):return[[not self.compare(IMG_STILL,(65+480*i+141*j,895,107+480*i+141*j,927),.1)for j in range(3)]for i in range(3)]
     def isApEmpty(self):return self.compare(IMG_APEMPTY,(800,50,1120,146))and fuse.reset()
     def isChooseFriend(self):return self.compare(IMG_CHOOSEFRIEND,(1628,314,1772,390))and fuse.reset()
     def isNoFriend(self):return self.compare(IMG_NOFRIEND,(369,545,1552,797),.1)and fuse.reset()
-    def getABQ(self):return[-1if self.compare(IMG_CARDSEALED,(43+386*i,667,345+386*i,845),.3)else(lambda x:x.index(max(x)))([numpy.mean(self.im[771:919,108+386*i:318+386*i,j])for j in(2,1,0)])for i in range(5)]
+    def getABQ(self):return[-1if self.compare(IMG_CARDSEALED,(43+386*i,667,345+386*i,845))else(lambda x:x.index(max(x)))([numpy.mean(self.im[771:919,108+386*i:318+386*i,j])for j in(2,1,0)])for i in range(5)]
     def getStage(self):return self.select(IMG_STAGE,(1290,14,1348,60))+1
     def getPortrait(self):return[self.im[640:740,195+480*i:296+480*i]for i in range(3)]
 def draw():
@@ -155,7 +162,6 @@ def main(appleCount=0,appleKind=0,battleFunc=oneBattle):
         printer('  Battle',battle)
         flush=True
         while True:
-            chk=Check(.1)
             if chk.isNoFriend():
                 if flush:
                     doit('\xBAJ',(500,1000))
