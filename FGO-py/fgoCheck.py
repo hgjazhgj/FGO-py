e2b8b0ab4a943d16c258e1d9ba700a69f24349cd
hgjazhgj <huguangjing0411@geektip.cc>
2021-09-07 21:50:12 +0800

    v7.0.0 全面屏适配,但又没有适配

--- FGO-py/fgoCheck.py
+++ FGO-py/fgoCheck.py
@@ -38,7 +38,8 @@ class Check(metaclass=logMeta(logger)):
     def find(self,img,rect=(0,0,1920,1080),threshold=.05):return(lambda loc:((rect[0]+loc[2][0]+(img.shape[1]>>1),rect[1]+loc[2][1]+(img.shape[0]>>1)),fuse.reset(self))[0]if loc[0]<threshold else None)(cv2.minMaxLoc(cv2.matchTemplate(self.im[rect[1]:rect[3],rect[0]:rect[2]],img,cv2.TM_SQDIFF_NORMED)))
     def isAddFriend(self):return self._compare(IMG.END,(243,863,745,982))
     def isApEmpty(self):return self._compare(IMG.APEMPTY,(906,897,1017,967))
-    def isBattleBegin(self):return self._compare(IMG.BATTLEBEGIN,(1673,959,1899,1069))
+    # modified (1673,959,1899,1069)
+    def isBattleBegin(self):return self._compare(IMG.BATTLEBEGIN,(1639,951,1865,1061))
     def isBattleContinue(self):return self._compare(IMG.BATTLECONTINUE,(1072,805,1441,895))
     def isBattleDefeated(self):return self._compare(IMG.DEFEATED,(445,456,702,523))
     def isBattleFinished(self):return self._compare(IMG.BOUND,(112,250,454,313))or self._compare(IMG.BOUNDUP,(987,350,1468,594))
@@ -47,7 +48,8 @@ class Check(metaclass=logMeta(logger)):
     def isGacha(self):return self._compare(IMG.GACHA,(973,960,1312,1052))
     def isHouguReady(self,that=None):return(lambda that:[not any(that._compare(j,(470+346*i,258,773+346*i,387),.4)for j in(IMG.HOUGUSEALED,IMG.CHARASEALED,IMG.CARDSEALED))and(numpy.mean(self.im[1019:1026,217+478*i:235+478*i])>55or numpy.mean(that.im[1019:1026,217+478*i:235+478*i])>55)for i in range(3)])(Check(.15)if that is None else that)
     def isListEnd(self,pos):return any(self._compare(i,(pos[0]-30,pos[1]-20,pos[0]+30,pos[1]+1),.25)for i in(IMG.LISTEND,IMG.LISTNONE))
-    def isMainInterface(self):return self._compare(IMG.MENU,(1630,950,1919,1079))
+    # modified (1630,950,1919,1079)
+    def isMainInterface(self):return self._compare(IMG.MENU,(1630,920,1919,1049))
     def isNextJackpot(self):return self._compare(IMG.JACKPOT,(1556,336,1859,397))
     def isNoFriend(self):return self._compare(IMG.NOFRIEND,(369,545,1552,797),.1)
     def isSkillReady(self):return[[not self._compare(IMG.STILL,(54+476*i+132*j,897,83+480*i+141*j,927),.1)for j in range(3)]for i in range(3)]
@@ -69,10 +71,12 @@ class Check(metaclass=logMeta(logger)):
     def getHP(self):return[self._ocr((300+476*i,930,439+476*i,965))for i in range(3)]
     def getNP(self):return[self._ocr((330+476*i,983,411+476*i,1020))for i in range(3)]
     def getPortrait(self):return[self.im[640:740,195+480*i:296+480*i]for i in range(3)]
+    # modified (1296,20,1342,56)
     @retryOnError()
-    def getStage(self):return self._select((IMG.STAGE1,IMG.STAGE2,IMG.STAGE3),(1296,20,1342,56),.5)+1
+    def getStage(self):return self._select((IMG.STAGE1,IMG.STAGE2,IMG.STAGE3),(1326,20,1372,56),.5)+1
+    # modified (1325,20,1372,56),.5)
     @retryOnError()
-    def getStageTotal(self):return self._select((IMG.STAGETOTAL1,IMG.STAGETOTAL2,IMG.STAGETOTAL3),(1325,20,1372,56),.5)+1
+    def getStageTotal(self):return self._select((IMG.STAGETOTAL1,IMG.STAGETOTAL2,IMG.STAGETOTAL3),(1350,20,1397,56),.5)+1
     def getTeamIndex(self):return cv2.minMaxLoc(cv2.matchTemplate(self.im[58:92,768:1152],IMG.TEAMINDEX,cv2.TM_SQDIFF_NORMED))[2][0]//37+1
     # def isEnemyDanger(self):raise NotImplementedError
     def getEnemyHPGauge(self):raise NotImplementedError
