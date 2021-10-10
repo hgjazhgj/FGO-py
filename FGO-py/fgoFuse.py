048f26d588e3262a4dbd63dabf50e19b1fcf63a8
hgjazhgj <huguangjing0411@geektip.cc>
2021-10-10 20:35:09 +0800

    v7.4.2

--- FGO-py/fgoFuse.py
+++ FGO-py/fgoFuse.py
@@ -3,23 +3,19 @@ from fgoLogging import getLogger
 logger=getLogger('Fuse')
 class Fuse:
     def __init__(self,fv=300,logsize=10):
-        self.__value=0
-        self.__max=fv
+        self.value=0
+        self.max=fv
         self.logsize=logsize
         self.log=[None]*logsize
         self.logptr=0
-    @property
-    def value(self):return self.__value
-    @property
-    def max(self):return self.__max
     def increase(self):
-        logger.debug(f'{self.__value}')
-        if self.__value>self.__max:
+        logger.debug(f'{self.value}')
+        if self.value>self.max:
             self.save()
             raise ScriptTerminate('Fused')
-        self.__value+=1
+        self.value+=1
     def reset(self,check=None):
-        self.__value=0
+        self.value=0
         if check is not None and check is not self.log[(self.logptr-1)%self.logsize]:
             self.log[self.logptr]=check
             self.logptr=(self.logptr+1)%self.logsize
