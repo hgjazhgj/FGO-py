e5f54937102a9764595e71999040d2c64be21d28
hgjazhgj <huguangjing0411@geektip.cc>
2021-12-28 02:15:22 +0800

    v7.10.0

--- FGO-py/fgoLogging.py
+++ FGO-py/fgoLogging.py
@@ -1,7 +1,7 @@
 import logging,time
 from copy import copy
 from functools import wraps
-logging.root.addHandler((lambda handler:(handler.setFormatter(logging.Formatter('[%(asctime)s][%(levelname)s]<%(name)s> %(message)s')),handler.setLevel(logging.DEBUG),handler)[-1])(logging.FileHandler(time.strftime('fgoLogs/Log_%Y-%m-%d_%H.%M.%S.txt'))))
+logging.root.addHandler((lambda handler:(handler.setFormatter(logging.Formatter('[%(asctime)s][%(levelname)s]<%(name)s> %(message)s')),handler.setLevel(logging.DEBUG),handler)[-1])(logging.FileHandler(time.strftime('fgoLog/Log_%Y-%m-%d_%H.%M.%S.txt'))))
 (lambda logger:(logger.setLevel(logging.DEBUG),logger.addHandler((lambda handler:(handler.setFormatter(type('ColoredFormatter',(logging.Formatter,),{'__init__':lambda self,*args,**kwargs:logging.Formatter.__init__(self,*args,**kwargs),'format':lambda self,record:((lambda record:(setattr(record,'levelname','\033[{}m[{}]'.format({'WARNING':'33','INFO':'34','DEBUG':'37','CRITICAL':'35','ERROR':'31'}.get(record.levelname,'0'),record.levelname)),logging.Formatter.format(self,record))[-1])(copy(record)))})('\033[32m[%(asctime)s]%(levelname)s\033[36m<%(name)s>\033[0m %(message)s')),handler.setLevel(logging.INFO),handler)[-1])(logging.StreamHandler()))))(logging.getLogger('fgo'))
 (lambda handler:(handler.setLevel(logging.INFO),handler.setFormatter(logging.getLogger('fgo').handlers[-1].formatter)))((lambda logger:(logger.setLevel(logging.DEBUG),logger)[-1])(logging.getLogger('airtest')).handlers[0])
 def getLogger(name):return logging.getLogger('fgo.'+name)
--- FGO-py/fgoServerChann.py
+++ FGO-py/fgoServerChann.py
@@ -3,7 +3,8 @@ class ServerChann:
     def __init__(self,scKey):
         self.scKey=scKey
     def __call__(self,msg):
-        return post(f'https://sctapi.ftqq.com/{self.scKey}.send',data={
+        try:return post(f'https://sctapi.ftqq.com/{self.scKey}.send',data={
             'title':'FGO-py',
             'desp':msg
         }).ok
+        except:return False
