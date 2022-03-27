80d8a2f02a0aad98b01213c8b2bf87e718b7b95c
hgjazhgj <huguangjing0411@geektip.cc>
2022-03-27 21:55:37 +0800

    v8.5.2

--- AidLux.sh
+++ AidLux.sh
@@ -1,14 +1,9 @@
-apt install -y gcc g++ clang make git zlib* openssl libssl* adb
-wget https://www.python.org/ftp/python/3.9.9/Python-3.9.9.tgz # http://npm.taobao.org/mirrors/python/3.9.9/Python-3.9.9.tgz
-tar zxvf Python-3.9.9.tgz
-cd Python-3.9.9
-./configure --enable-optimizations
-make -j8
-make altinstall
-# pip3.9 config set global.index-url https://mirrors.aliyun.com/pypi/simple/
-# pip3.9 config set install.trusted-host mirrors.aliyun.com
+aid install python-3.9.10
+apt install git adb
+pip3.9 config set global.index-url https://mirrors.aliyun.com/pypi/simple/
+pip3.9 config set install.trusted-host mirrors.aliyun.com
 pip3.9 install airtest
-cp /usr/bin/adb /usr/local/lib/python3.9/site-packages/airtest/core/android/static/adb/linux/
-git clone https://github.com/hgjazhgj/FGO-py.git # https://gitee.com/hgjazhgj/FGO-py.git
+ln -s /usr/lib/android-sdk/platform-tools/adb /usr/local/python-3.9.10/lib/python3.9/site-packages/airtest/core/android/static/adb/linux/adb
+git clone https://github.com/hgjazhgj/FGO-py.git # https://e.coding.net/hgjazhgj/fgo-py/FGO-py.git
 cd FGO-py/FGO-py
 python3.9 fgo.py cli
--- FGO-py/fgoKernel.py
+++ FGO-py/fgoKernel.py
@@ -80,59 +80,68 @@ def synthesis():
         if Detect().isSynthesisFinished():break
         device.perform('  KK\xBB\xBB\xBB\xBB\xBB\xBB\xBB\xBB\xBB\xBB\xBB\xBB\xBB\xBB\xBB',(800,300,300,1000,150,150,150,150,150,150,150,150,150,150,150,150,150,150,150))
         while not Detect().isSynthesisBegin():device.press('\xBB')
-class Battle:
+class Turn:
     skillInfo=[[[0,0,0,7],[0,0,0,7],[0,0,0,7]],[[0,0,0,7],[0,0,0,7],[0,0,0,7]],[[0,0,0,7],[0,0,0,7],[0,0,0,7]],[[0,0,0,7],[0,0,0,7],[0,0,0,7]],[[0,0,0,7],[0,0,0,7],[0,0,0,7]],[[0,0,0,7],[0,0,0,7],[0,0,0,7]]]
     houguInfo=[[1,7],[1,7],[1,7],[1,7],[1,7],[1,7]]
     masterSkill=[[0,0,0,7],[0,0,0,7],[0,0,0,0,7]]
     def __init__(self):
-        Battle.friendInfo=[[[-1,-1,-1,-1],[-1,-1,-1,-1],[-1,-1,-1,-1]],[-1,-1]]
-        self.turn=0
+        Turn.friendInfo=[[[-1,-1,-1,-1],[-1,-1,-1,-1],[-1,-1,-1,-1]],[-1,-1]]
         self.stage=0
         self.stageTurn=0
         self.servant=[0,1,2]
         self.orderChange=[0,1,2,3,4,5]
         self.masterSkillReady=[True,True,True]
+    def __call__(self,turn):
+        self.stage,self.stageTurn=[t:=Detect(.2).getStage(),1+self.stageTurn*(self.stage==t)]
+        self.friend=Detect.cache.isServantFriend()
+        if turn==1:
+            Detect.cache.setupServantDead(self.friend)
+            self.stageTotal=Detect.cache.getStageTotal()
+        else:self.servant=(lambda m,p:[m+p.index(i)+1 if i in p else self.servant[i]for i in range(3)])(max(self.servant),(lambda dead:[i for i in range(3)if self.servant[i]<6 and dead[i]])(Detect.cache.isServantDead(self.friend)))
+        logger.info(f'Turn {turn} Stage {self.stage} StageTurn {self.stageTurn} {self.servant}')
+        if self.stageTurn==1:device.perform('\x67\x68\x69'[numpy.argmax(Detect.cache.getEnemyHP())]+'\xBB',(800,500))
+        self.dispatchSkill()
+        device.perform(' ',(2100,))
+        device.perform(self.selectCard(),(300,300,2300,1300,6000))
+    def dispatchSkill(self):
+        while(s:=(lambda skill:[(self.getSkillInfo(i,j,3),0,(i,j))for i in range(3)if self.servant[i]<6 for j in range(3)if skill[i][j]and(t:=self.getSkillInfo(i,j,0))and min(t,self.stageTotal)<<8|self.getSkillInfo(i,j,1)<=self.stage<<8|self.stageTurn])(Detect.cache.isSkillReady())+[(self.masterSkill[i][-1],1,(i,))for i in range(3)if self.masterSkillReady[i]and self.stage==min(self.masterSkill[i][0],self.stageTotal)and self.stageTurn==self.masterSkill[i][1]]):
+            _,cast,arg=min(s,key=lambda x:x[0])
+            [self.castServantSkill,self.castMasterSkill][cast](*arg)
+            device.perform('\x08',(2300,))
+            while not Detect().isTurnBegin():pass
+            Detect(.5)
+    @logit(logger,logging.INFO)
+    def selectCard(self):return''.join((lambda hougu,sealed,color,resist,critical:['678'[i]for i in sorted((i for i in range(3)if hougu[i]),key=lambda x:self.getHouguInfo(x,1))]+['12345'[i]for i in sorted(range(5),key=(lambda x:-color[x]*resist[x]*(not sealed[x])*(1+critical[x])))]if any(hougu)else(lambda group:['12345'[i]for i in(lambda choice:choice+tuple({0,1,2,3,4}-set(choice)))(logger.debug('cardRank'+','.join(('  'if i%5 else'\n')+f'({j}, {k:5.2f})'for i,(j,k)in enumerate(sorted([(card,(lambda colorChain,firstCardBonus:sum((firstCardBonus+[1.,1.2,1.4][i]*color[j])*(1+critical[j])*resist[j]*(not sealed[j])for i,j in enumerate(card))+(not any(sealed[i]for i in card))*(4.8*colorChain+(firstCardBonus+1.)*(3 if colorChain else 1.8)*(len({group[i]for i in card})==1)*resist[card[0]]))(len({color[i]for i in card})==1,.3*(color[card[0]]==1.1)))for card in permutations(range(5),3)],key=lambda x:-x[1]))))or max(permutations(range(5),3),key=lambda card:(lambda colorChain,firstCardBonus:sum((firstCardBonus+[1.,1.2,1.4][i]*color[j])*(1+critical[j])*resist[j]*(not sealed[j])for i,j in enumerate(card))+(not any(sealed[i]for i in card))*(4.8*colorChain+(firstCardBonus+1.)*(3 if colorChain else 1.8)*(len({group[i]for i in card})==1)*resist[card[0]]))(len({color[i]for i in card})==1,.3*(color[card[0]]==1.1))))])(Detect.cache.getCardGroup()))([self.servant[i]<6 and j and(t:=self.getHouguInfo(i,0))and self.stage>=min(t,self.stageTotal)for i,j in enumerate(Detect().isHouguReady())],Detect.cache.isCardSealed(),Detect.cache.getCardColor(),Detect.cache.getCardResist(),Detect.cache.getCriticalRate()))
+    def getSkillInfo(self,pos,skill,arg):return self.friendInfo[0][skill][arg]if self.friend[pos]and self.friendInfo[0][skill][arg]>=0 else self.skillInfo[self.orderChange[self.servant[pos]]][skill][arg]
+    def getHouguInfo(self,pos,arg):return self.friendInfo[1][arg]if self.friend[pos]and self.friendInfo[1][arg]>=0 else self.houguInfo[self.orderChange[self.servant[pos]]][arg]
+    def castServantSkill(self,pos,skill):
+        device.perform(('ASD','FGH','JKL')[pos][skill],(300,))
+        if t:=self.getSkillInfo(pos,skill,2):device.perform('234'[t-1],(300,))
+    def castMasterSkill(self,skill):
+        self.masterSkillReady[skill]=False
+        device.perform('Q'+'WER'[skill],(300,300))
+        if self.masterSkill[skill][2]:
+            if skill==2 and self.masterSkill[2][3]:
+                if self.masterSkill[2][2]-1 not in self.servant or self.masterSkill[2][3]-1 in self.servant:return device.perform('\xBB',(300,))
+                p=self.servant.index(self.masterSkill[2][2]-1)
+                device.perform(('TYUIOP'[p],'TYUIOP'[self.masterSkill[2][3]-max(self.servant)+1],'Z'),(300,300,2600))
+                self.orderChange[self.masterSkill[2][2]-1],self.orderChange[self.masterSkill[2][3]-1]=self.orderChange[self.masterSkill[2][3]-1],self.orderChange[self.masterSkill[2][2]-1]
+                device.perform('\x08',(2300,))
+                while not Detect().isTurnBegin():pass
+                self.friend=Detect(.5).isServantFriend()
+                Detect.cache.setupServantDead(self.friend)
+                return
+            device.perform('234'[self.masterSkill[skill][2]-1],(300,))
+class Battle:
+    def __init__(self,turnClass=Turn):
+        self.turn=0
+        self.turnProc=turnClass()
         self.rainbowBox=False
     def __call__(self):
         while True:
             if Detect(0,.3).isTurnBegin():
                 self.turn+=1
-                self.stage,self.stageTurn=[t:=Detect(.2).getStage(),1+self.stageTurn*(self.stage==t)]
-                self.friend=Detect.cache.isServantFriend()
-                Detect.cache.getHP(),Detect.cache.getNP()
-                if self.turn==1:
-                    Detect.cache.setupServantDead(self.friend)
-                    self.stageTotal=Detect.cache.getStageTotal()
-                else:self.servant=(lambda m,p:[m+p.index(i)+1 if i in p else self.servant[i]for i in range(3)])(max(self.servant),(lambda dead:[i for i in range(3)if self.servant[i]<6 and dead[i]])(Detect.cache.isServantDead(self.friend)))
-                logger.info(f'Turn {self.turn} Stage {self.stage} StageTurn {self.stageTurn} {self.servant}')
-                if self.stageTurn==1:device.perform('\x67\x68\x69'[numpy.argmax(Detect.cache.getEnemyHP())]+'\xBB',(800,500))
-                while(s:=(lambda skill:[(self.getSkillInfo(i,j,3),0,(i,j))for i in range(3)if self.servant[i]<6 for j in range(3)if skill[i][j]and(t:=self.getSkillInfo(i,j,0))and min(t,self.stageTotal)<<8|self.getSkillInfo(i,j,1)<=self.stage<<8|self.stageTurn])(Detect.cache.isSkillReady())+[(self.masterSkill[i][-1],1,i)for i in range(3)if self.masterSkillReady[i]and self.stage==min(self.masterSkill[i][0],self.stageTotal)and self.stageTurn==self.masterSkill[i][1]]):
-                    _,cast,arg=min(s,key=lambda x:x[0])
-                    if cast==0:
-                        device.perform(('ASD','FGH','JKL')[arg[0]][arg[1]],(300,))
-                        if t:=self.getSkillInfo(*arg,2):device.perform('234'[t-1],(300,))
-                    elif cast==1:
-                        self.masterSkillReady[arg]=False
-                        device.perform('Q'+'WER'[arg],(300,300))
-                        if self.masterSkill[arg][2]:
-                            if arg==2 and self.masterSkill[2][3]:
-                                if self.masterSkill[2][2]-1 not in self.servant or self.masterSkill[2][3]-1 in self.servant:
-                                    device.perform('\xBB',(300,))
-                                    continue
-                                p=self.servant.index(self.masterSkill[2][2]-1)
-                                device.perform(('TYUIOP'[p],'TYUIOP'[self.masterSkill[2][3]-max(self.servant)+1],'Z'),(300,300,2600))
-                                self.orderChange[self.masterSkill[2][2]-1],self.orderChange[self.masterSkill[2][3]-1]=self.orderChange[self.masterSkill[2][3]-1],self.orderChange[self.masterSkill[2][2]-1]
-                                device.perform('\x08',(2300,))
-                                while not Detect().isTurnBegin():pass
-                                self.friend=Detect(.5).isServantFriend()
-                                Detect.cache.setupServantDead(self.friend)
-                                continue
-                            device.perform('234'[self.masterSkill[arg][2]-1],(300,))
-                    device.perform('\x08',(2300,))
-                    while not Detect().isTurnBegin():pass
-                    Detect(.5)
-                device.perform(' ',(2100,))
-                device.perform(self.selectCard(),(300,300,2300,1300,6000))
+                self.turnProc(self.turn)
             elif Detect.cache.isSpecialDropSuspended():
                 schedule.checkKizunaReisou()
                 logger.warning('Kizuna Reisou')
@@ -151,10 +160,6 @@ class Battle:
                 schedule.checkDefeated()
                 return 0
             device.perform('\xBB\x08',(100,100))
-    @logit(logger,logging.INFO)
-    def selectCard(self):return''.join((lambda hougu,sealed,color,resist,critical:['678'[i]for i in sorted((i for i in range(3)if hougu[i]),key=lambda x:self.getHouguInfo(x,1))]+['12345'[i]for i in sorted(range(5),key=(lambda x:-color[x]*resist[x]*(not sealed[x])*(1+critical[x])))]if any(hougu)else(lambda group:['12345'[i]for i in(lambda choice:choice+tuple({0,1,2,3,4}-set(choice)))(logger.debug('cardRank'+','.join(('  'if i%5 else'\n')+f'({j}, {k:5.2f})'for i,(j,k)in enumerate(sorted([(card,(lambda colorChain,firstCardBonus:sum((firstCardBonus+[1.,1.2,1.4][i]*color[j])*(1+critical[j])*resist[j]*(not sealed[j])for i,j in enumerate(card))+(not any(sealed[i]for i in card))*(4.8*colorChain+(firstCardBonus+1.)*(3 if colorChain else 1.8)*(len({group[i]for i in card})==1)*resist[card[0]]))(len({color[i]for i in card})==1,.3*(color[card[0]]==1.1)))for card in permutations(range(5),3)],key=lambda x:-x[1]))))or max(permutations(range(5),3),key=lambda card:(lambda colorChain,firstCardBonus:sum((firstCardBonus+[1.,1.2,1.4][i]*color[j])*(1+critical[j])*resist[j]*(not sealed[j])for i,j in enumerate(card))+(not any(sealed[i]for i in card))*(4.8*colorChain+(firstCardBonus+1.)*(3 if colorChain else 1.8)*(len({group[i]for i in card})==1)*resist[card[0]]))(len({color[i]for i in card})==1,.3*(color[card[0]]==1.1))))])(Detect.cache.getCardGroup()))([self.servant[i]<6 and j and(t:=self.getHouguInfo(i,0))and self.stage>=min(t,self.stageTotal)for i,j in enumerate(Detect().isHouguReady())],Detect.cache.isCardSealed(),Detect.cache.getCardColor(),Detect.cache.getCardResist(),Detect.cache.getCriticalRate()))
-    def getSkillInfo(self,pos,skill,arg):return self.friendInfo[0][skill][arg]if self.friend[pos]and self.friendInfo[0][skill][arg]>=0 else self.skillInfo[self.orderChange[self.servant[pos]]][skill][arg]
-    def getHouguInfo(self,pos,arg):return self.friendInfo[1][arg]if self.friend[pos]and self.friendInfo[1][arg]>=0 else self.houguInfo[self.orderChange[self.servant[pos]]][arg]
 class Main:
     teamIndex=0
     def __init__(self,appleTotal=0,appleKind=0,battleClass=Battle):
@@ -165,7 +170,7 @@ class Main:
         self.battleCount=0
     def __call__(self):
         while True:
-            self.battleFunc=self.battleClass()
+            self.battleProc=self.battleClass()
             while True:
                 if Detect(.3,.3).isMainInterface():
                     device.press('8')
@@ -187,7 +192,7 @@ class Main:
                 device.press('\xBB')
             self.battleCount+=1
             logger.info(f'Battle {self.battleCount}')
-            if self.battleFunc():device.perform('      ',(200,200,200,200,200,200))
+            if self.battleProc():device.perform('      ',(200,200,200,200,200,200))
             else:device.perform('BIK',(500,500,500))
             schedule.checkTerminateLater()
     @logit(logger,logging.INFO)
@@ -209,7 +214,7 @@ class Main:
             timer=time.time()
             while True:
                 for i in(i for i,j in friendImg.items()if(lambda pos:pos and(device.touch(pos),True)[-1])(Detect.cache.findFriend(j))):
-                    Battle.friendInfo=(lambda r:(lambda p:([[[-1 if p[i*4+j]=='X'else int(p[i*4+j],16)for j in range(4)]for i in range(3)],[-1 if p[i+12]=='X'else int(p[i+12],16)for i in range(2)]]))(r.group())if r else[[[-1,-1,-1,-1],[-1,-1,-1,-1],[-1,-1,-1,-1]],[-1,-1]])(re.match('([0-9X]{3}[0-9A-FX]){3}[0-9X][0-9A-FX]$',i.replace('-','')[-14:].upper()))
+                    Turn.friendInfo=(lambda r:(lambda p:([[[-1 if p[i*4+j]=='X'else int(p[i*4+j],16)for j in range(4)]for i in range(3)],[-1 if p[i+12]=='X'else int(p[i+12],16)for i in range(2)]]))(r.group())if r else[[[-1,-1,-1,-1],[-1,-1,-1,-1],[-1,-1,-1,-1]],[-1,-1]])(re.match('([0-9X]{3}[0-9A-FX]){3}[0-9X][0-9A-FX]$',i.replace('-','')[-14:].upper()))
                     return i
                 if Detect.cache.isFriendListEnd():break
                 device.swipe((800,900,800,300))
@@ -222,13 +227,6 @@ class Main:
                     schedule.sleep(10)
                     device.perform('\xBAK',(500,1000))
 class UserScript:
-    def __call__(self):
-        while not Detect(0,.3).isTurnBegin():device.press('\xBB')
-        # # BX WCBA 极地用迦勒底制服
-        # #                                      A    D    F    2    G   H    2   J   2    K    L    2   Q   E   2     _   6   5    4
-        # device.perform('ADF2GH2J2KL2QE2 654',(3000,3000,350,3000,3000,350,3000,350,3000,3000,350,3000,300,350,3000,2400,350,350,10000))
-        # # Hikari Nobu      Kintoki wcba atorasu
-        # #                                   Q   E    2    A    F    2    G   H    2   J    2    K   L    2    _   6   5    4
-        # device.perform('QE2AF2GH2J2KL2 654',(300,350,3000,3000,350,3000,3000,350,3000,350,3000,3000,350,3000,2400,350,350,10000))
-        device.perform('QE2',(300,350,3000))
-        return Battle()()
+    def __call__(self):return Battle(Xjbd)()
+class Xjbd(Turn):
+    pass
