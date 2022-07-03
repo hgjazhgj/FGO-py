88956d61c9b47003d3275d20b60687176a898f8f
hgjazhgj <hgjazhgj[at]gmail.com>
2022-07-03 20:50:15 +0800

    v9.0.2

--- FGO-py/fgoCli.py
+++ FGO-py/fgoCli.py
@@ -192,25 +192,32 @@ Some commands support <command> [<subcommand> ...] {{-h, --help}} for further in
         fgoKernel.bench(max(3,arg.number),arg.input,arg.output)
 
 ArgError=type('ArgError',(Exception,),{})
+def validator(type,func,desc='\b'):
+    def f(x):
+        x=type(x)
+        if not func(x):raise ValueError
+        return x
+    f.__name__=desc
+    return f
 class ArgParser(argparse.ArgumentParser):
     def exit(self,status=0,message=None):raise ArgError(message)
 
 parser_battle=ArgParser(prog='battle',description=Cmd.do_battle.__doc__)
-parser_battle.add_argument('-s','--sleep',help='Sleep before run (default: %(default)s)',type=float,default=0)
+parser_battle.add_argument('-s','--sleep',help='Sleep before run (default: %(default)s)',type=validator(float,lambda x:x>=0,'nonnegative'),default=0)
 
 parser_main=ArgParser(prog='main',description=Cmd.do_main.__doc__)
-parser_main.add_argument('appleCount',help='Apple Count (default: %(default)s)',type=int,default=0,nargs='?')
+parser_main.add_argument('appleCount',help='Apple Count (default: %(default)s)',type=validator(int,lambda x:x>=0,'nonnegative int'),default=0,nargs='?')
 parser_main.add_argument('appleKind',help='Apple Kind (default: %(default)s)',type=str.lower,choices=['gold','silver','bronze','quartz'],default='gold',nargs='?')
 parser_main.add_argument('battleClass',help='Battle Class (default: %(default)s)',choices=['Battle','UserScript'],default='Battle',nargs='?')
-parser_main.add_argument('-s','--sleep',help='Sleep before run (default: %(default)s)',type=float,default=0)
+parser_main.add_argument('-s','--sleep',help='Sleep before run (default: %(default)s)',type=validator(float,lambda x:x>=0,'nonnegative'),default=0)
 
 parser_connect=ArgParser(prog='connect',description=Cmd.do_connect.__doc__)
-parser_connect.add_argument('-l','--list',help='List all available devicess',action='store_true')
+parser_connect.add_argument('-l','--list',help='List all available devices',action='store_true')
 parser_connect.add_argument('name',help='Device name',default='',nargs='?')
 
 parser_teamup=ArgParser(prog='teamup',description=Cmd.do_teamup.__doc__)
 parser_teamup_=parser_teamup.add_subparsers(title='subcommands',required=True,dest='subcommand_0')
-parser_teamup_load=parser_teamup_.add_parser('load',help='Load a team to surrent')
+parser_teamup_load=parser_teamup_.add_parser('load',help='Load a team to current')
 parser_teamup_load.add_argument('name',help='Teamup Name (default: %(default)s)',default='DEFAULT',nargs='?')
 parser_teamup_save=parser_teamup_.add_parser('save',help='Save all teams')
 parser_teamup_clear=parser_teamup_.add_parser('clear',help='Clear current team')
@@ -229,7 +236,7 @@ parser_teamup_set_index.add_argument('value',help='Team index (0-10)',type=int,c
 
 parser_call=ArgParser(prog='call',description=Cmd.do_call.__doc__)
 parser_call.add_argument('func',help='Additional feature name',choices=['gacha','jackpot','mail','synthesis'])
-parser_call.add_argument('-s','--sleep',help='Sleep several seconds before run (default: %(default)s)',type=float,default=0)
+parser_call.add_argument('-s','--sleep',help='Sleep several seconds before run (default: %(default)s)',type=validator(float,lambda x:x>=0,'nonnegative'),default=0)
 
 parser_169=ArgParser(prog='169',description=Cmd.do_169.__doc__)
 parser_169.add_argument('action',help='Action',type=str.lower,choices=['invoke','revoke'])
@@ -239,7 +246,7 @@ parser_press.add_argument('button',help='Button',type=str.upper)
 parser_press.add_argument('-c','--code',help='Use virtual key code',action='store_true')
 
 parser_bench=ArgParser(prog='bench',description=Cmd.do_bench.__doc__)
-parser_bench.add_argument('-n','--number',help='Number of runs (default: %(default)s)',type=int,default=20)
+parser_bench.add_argument('-n','--number',help='Number of runs (default: %(default)s)',type=validator(int,lambda x:x>=3,'not-less-than-3 int'),default=20)
 parser_bench.add_argument('-i','--input',help='Bench touch, if neither -i nor -o specified, bench them both',action='store_true')
 parser_bench.add_argument('-o','--output',help='Bench screenshot, if neither -i nor -o specified, bench them both',action='store_true')
 
