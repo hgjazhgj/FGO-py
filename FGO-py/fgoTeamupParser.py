7ef8847d4e4b1695e51ad9560302eb6e26bab39e
hgjazhgj <hgjazhgj[at]gmail.com>
2023-04-19 20:15:28 +0800

    filter-branch rebuild gh-pages purge

--- FGO-py/fgoTeamupParser.py
+++
@@ -1,2 +0,0 @@
-from configparser import ConfigParser
-IniParser=type('IniParser',(ConfigParser,),{'__init__':lambda self,file:(ConfigParser.__init__(self),self.read(file,'utf-8'))[0],'optionxform':lambda self,optionstr:optionstr})
