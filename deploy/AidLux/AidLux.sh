5bafcbbe2023bbb03c56335ed9b07ba475a905e6
hgjazhgj <40085173+hgjazhgj@users.noreply.github.com>
2023-06-02 03:17:22 +0800

    Upd deploy due to v11.3.0

--- deploy/AidLux/AidLux.sh
+++ deploy/AidLux/AidLux.sh
@@ -1,9 +1,9 @@
+#!/bin/bash
 aid install python-3.9.10
 apt install -y git adb
 # pip3.9 config set global.index-url https://mirrors.aliyun.com/pypi/simple/
 # pip3.9 config set install.trusted-host mirrors.aliyun.com
 pip3.9 install airtest
-ln -s /usr/lib/android-sdk/platform-tools/adb /usr/local/python-3.9.10/lib/python3.9/site-packages/airtest/core/android/static/adb/linux/adb
-git clone --depth=1 https://github.com/hgjazhgj/FGO-py.git # https://gitee.com/hgjazhgj/FGO-py.git
+git clone https://github.com/hgjazhgj/FGO-py.git # https://gitee.com/hgjazhgj/FGO-py.git
 cd FGO-py/FGO-py
 python3.9 fgo.py cli
