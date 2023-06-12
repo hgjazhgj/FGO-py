5b2c35ccd993b2072be30e75fcd4f7536f264a51
hgjazhgj <40085173+hgjazhgj@users.noreply.github.com>
2023-06-13 04:19:38 +0800

    Fix: cd /d for fpy and alas in different disk

--- deploy/AzurLaneAutoScript/examples/portable.launch.bat
+++ deploy/AzurLaneAutoScript/examples/portable.launch.bat
@@ -1,6 +1,7 @@
 @echo off
+echo %~dp0
 set "_root=%~dp0..\..\..\"
 set "PATH=%_root%Python39;%_root%Python39\Scripts;%_root%Git\mingw64\bin;%PATH%"
-cd "%_root%FGO-py\FGO-py"
+cd /d "%_root%FGO-py\FGO-py"
 git pull --no-rebase origin master
 python fgo.py cli --no-color
